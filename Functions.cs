0aedd5e1e469e875588198d298514a567a720c90
T9RB <98963131+T9RB@users.noreply.github.com>
2023-05-18 10:27:46 +0700

    fix errors

--- Functions.cs
+++ Functions.cs
@@ -2,11 +2,7 @@
 
 public class Functions
 {
-    public double Plus(double a, double b)
-    {
-        double result = a + b;
-        return result;
-    }
+  
     public double Minus(double a, double b)
     {
         double result = a - b;
