2c4530fcafea7100343dc47186fa3238549b4672
T9RB <m9kish.play.tomsk@gmail.com>
2023-05-18 09:02:45 +0700

    Add 'Plus' function

--- Program.cs
+++ Program.cs
@@ -2,3 +2,14 @@
 using Miapo_Rybalkin;
 
 Functions functions = new();
+
+double a = 0;
+double b = 0;
+
+Console.WriteLine("Введите первое число: ");
+a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+b = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Результат {functions.Plus(a, b)}");
