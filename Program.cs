5912fe7462690c14ba46ddb5583fa6da95150011
T9RB <98963131+T9RB@users.noreply.github.com>
2023-05-18 10:30:01 +0700

    fix errors

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 
 Functions functions = new();
 
-string promit;
 double a = 0;
 double b = 0;
 
@@ -13,33 +12,14 @@ Console.WriteLine("Введите второе число: ");
 
 b = double.Parse(Console.ReadLine() ?? string.Empty);
 
-Console.WriteLine("Выберите функцию: 1. Плюс, 2. Минус");
-promit = Console.ReadLine() ?? string.Empty;
-
-if (promit == "1")
-{
-    //Вызов функции Plus с помощью try-catch
+ //Вызов функции Multiplication с помощью try-catch
     try
     {
-        Console.WriteLine($"Результат {functions.Plus(a, b)}");
+        Console.WriteLine($"Результат {functions.Multiplication(a, b)}");
     }
     catch(Exception e)
     {
         Console.WriteLine(e);
     }
-}
-
-else if (promit == "2")
-{
-    //Вызов функции Minus с помощью try-catch
-    try
-    {
-        Console.WriteLine($"Результат {functions.Minus(a, b)}");
-    }
-    catch(Exception e)
-    {
-        Console.WriteLine(e);
-    }
-}
 
 
